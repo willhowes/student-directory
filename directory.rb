6ead0d9dff943134b7394dd23e62f08df0ae47cd
eldenhowes <williameldenhowes@gmail.com>
2019-06-13 10:44:37 +0100

    allow for user to say which file to save student list to

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 @students = [] # an array accessible to all methods
 
 def print_menu
-  puts "1. Input the students\n2. Show the students\n"\
+  puts "1. Input the students\n2. Print the student list\n"\
   "3. Save the list to 'students.csv'\n4. Load the list from 'students.csv'\n"\
   "9. Exit"
 end
@@ -24,6 +24,8 @@ def process_user_selection(selection)
   when '4'
     load_students
   when '9'
+    puts "\nEXITING GAME..."
+    sleep 1
     exit
   else
     puts "I don't know what you meant, try again"
@@ -31,6 +33,7 @@ def process_user_selection(selection)
 end
 
 def input_students
+  puts "\n ---INPUT STUDENTS SELECTED---"
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first names
@@ -46,6 +49,7 @@ def input_students
 end
 
 def print_student_list
+  puts "\n ---PRINT STUDENT LIST SELECTED---"
   puts "\nThe students of Villains Academy \n------------"
   if @students.length >= 1
     @students.each do |student|
@@ -53,6 +57,7 @@ def print_student_list
     end
   end
   puts "\nOverall, we have #{@students.count} great students\n------------\n\n"
+  sleep 2.5
 end
 
 def add_students
@@ -60,15 +65,21 @@ def add_students
 end
 
 def save_students
-  file = File.open("students.csv", "w")
+  puts "\n---SAVE STUDENTS SELECTED---"
+  puts "Please confirm the file name to save to: "
+  user_filename = gets.chomp
+  user_filename = 'students.csv' if user_filename == ""
+  file = File.open(user_filename, "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]].join(",")
     file.puts student_data
   end
+  puts "\nList of students saved to #{user_filename}...\n\n"
   file.close
 end
 
 def load_students(filename = 'students.csv')
+  puts "\n---LOAD STUDENTS SELECTED---"
   file = File.open(filename, 'r')
   file.readlines.each do |line|
     @name, cohort = line.chomp.split(',')
